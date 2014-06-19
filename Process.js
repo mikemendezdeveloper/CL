fab8bcb5eb0b26229da40eeba3a9e3cc0c8e3dfe
Michael Mendez <michael.mendez@sephora.com>
2014-06-19 16:15:07 -0700

    making progress...

---
+++ Process.js
@@ -0,0 +1,7 @@
+Process
+
+1.) Mouse wheel scrolls
+2.) Get the new top dis value
+3.) Use that value as a destination to reach
+4.) Write a function that steps up to that destination with a timeout
+5.) Allow another mouse wheel scroll to cancel that timeout and start the process over.
