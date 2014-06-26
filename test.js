3f4534d30c160aba1f5341bcbc88328d9f2a0434
Michael Mendez <michael.mendez@sephora.com>
2014-06-26 11:42:33 -0700

    Working Well: Test.js

--- test.js
+++ test.js
@@ -2,7 +2,8 @@
 var movingElems = $(".parallax"),
 	scrollPos = Math.min($(window).scrollTop()),
 	newPos = 0,
-	scrollDelta = 0;
+	scrollDelta = 0,
+	timingValue = 20;
 
 
 
@@ -33,45 +34,49 @@ function move(scrollDelta, scrollPos){
 									console.log("before" + elem.sideOffset);
 								}
 								if (i == 0){console.log("scrollPos: " + scrollPos);console.log("startOffset: " + elem.startOffset);}
-
-		if (Math.abs(elem.extraOffset) > 0){ //Use all extra offset first
-			if (scrollPos >= elem.startOffset) {
-				elem.extraOffset += scrollDelta;
-			}
-			if (scrollPos < elem.startOffset){elem.extraOffset = 0;} //don't allow negative value
+		
+		if (scrollPos <= elem.startOffset){
+			elem.extraOffset = 0;
 		}
 		else {
-			elem.sideOffset += scrollDelta; //if extra is used up, start moving
+			elem.extraOffset = (scrollPos - elem.startOffset); //start tracking extra offset
+			if (elem.sideOffset != 0){//if its not already zero, set it to 0 and move it there.
+				elem.sideOffset = 0;
+
+				if (elem.side === "left"){
+					self.stop(true, false).animate({left: elem.sideOffset}, 300, "linear");
+				}
+				else {
+					self.stop(true, false).animate({right: elem.sideOffset}, 300, "linear");
+				}
+			}
 		}
 
+		if (Math.abs(elem.extraOffset) == 0){ //If there is no extraOffset then move
+		
+			elem.sideOffset += scrollDelta; 
 								if (i == 0){console.log("after" + elem.sideOffset);}
+			if (elem.sideOffset > 0){
+				elem.sideOffset = 0;
+			}
+			if (elem.sideOffset < elem.maxOffset){elem.sideOffset = elem.maxOffset;}
 
-		if (elem.sideOffset > 0){
-			elem.sideOffset = 0;
-			elem.extraOffset += scrollDelta; //save extra offset
-		}
-								if (i == 0){console.log("extraOffset: " + elem.extraOffset);}
-
-		if (elem.sideOffset < elem.maxOffset){elem.sideOffset = elem.maxOffset}
-
-		/*
-		* Only move if amount you are about to move by, is either larger than
-		* the initial offset, or smaller than max offset. That might be backwards...
-		*/
+			//Actually Move the element
 			if (elem.side === "left"){
 				self.stop(true, false).animate({left: elem.sideOffset}, 300, "linear");
 			}
 			else {
 				self.stop(true, false).animate({right: elem.sideOffset}, 300, "linear");
 			}
-		
+		}
+							if (i == 0){console.log("sideOffset: " + elem.sideOffset + " extraOffset: " + elem.extraOffset);}
 	})
 };
 
 $(window).scroll(function (){ 
 	newPos = Math.min($(window).scrollTop());
 
-	scrollDelta = newPos - scrollPos; //wrap in Math.abs() to get absolute value.
+	scrollDelta = newPos - scrollPos;
 
 	scrollPos = newPos;//Remember to update scrollPos
 
