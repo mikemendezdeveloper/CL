48ec4b191bc6970c4f422ad728eb7129edbb0459
Michael Mendez <michael.mendez@sephora.com>
2014-06-30 17:01:19 -0700

    Works well cross browser
    Make sure when testing in other browsers that the code is in BCC and
    not linked to in any way. Otherwise it probably won’t work.

--- test.js
+++ test.js
@@ -1,129 +1,140 @@
-
-var movingElems = $(".parallax"),
-	bottle = $("#bottle"),
-	scrollPos = Math.min($(window).scrollTop()),
-	newPos = 0,
-	scrollDelta = 0,
-	timingValue = 180,
-	initialScrollPos,
-	specialSpeed = .2;
-
-//Used for initialization, and then upon every scroll event.
-function scrolling (pageLoad) {
-	newPos = Math.min($(window).scrollTop());
-
-	scrollDelta = newPos - scrollPos;
-
-	scrollPos = newPos;//Remember to update scrollPos
-
-	pageLoad ? initialScrollPos = newPos : 0;
-
-	if (scrollDelta > 0 || pageLoad){ //Only move when scrolling down the page or on page load
-		moveLogic(scrollDelta, scrollPos);
-	}
-};
+$( document ).ready(function() {
+	/*Test before running the rest of the script */
 
 
-//Initialize all elements
-movingElems.each(function(i, elem){
-	var self = $(this);
-	
-	var initial = self.offset().top - timingValue;
+	function initParallax(){
+		var movingElems = $(".parallax"),
+		bottle = $("#bottle"),
+		scrollPos = 0,
+		newPos = 0,
+		scrollDelta = 0,
+		timingValue = 180,
+		initialScrollPos,
+		specialSpeed = .2;
 
-	if ( self.data("wait") ){initial += self.data("wait");}
-	//get data attribute for extra offset?
-	//Calculate original position based on dist from top of page?
-	elem.sideOffset = elem.maxOffset = initial * -1;
-	elem.startDistance = initial;
-	elem.speed = 1;
-	//Set the left/right offset of the element based on its topOffset
-	elem.side = self.hasClass("left") ? "left" : "right";
+		//Used for initialization, and then upon every scroll event.
+		function scrolling (pageLoad) {
+			newPos = Math.min($(window).scrollTop());
 
-	//Initialize an "extraOffset" property for keeping track past 0
-	elem.extraOffset = 0;
-	elem.sideOffset = elem.maxOffset = initial * -1; //!!This is hardcoded to get around a bug.
-	
-	//only for the bottle
-	if (self.hasClass("bottle")){
-		elem.side = "top";
-		if ( self.is("#bottle") ){
-			elem.startDistance = 1600;
-			elem.sideOffset = 1600;
-			elem.maxOffset = 2120;
-			elem.speed = specialSpeed;
-		}
-	}
-	if ( self.is("#stem") ){
-		elem.maxOffset = 215;
+			scrollDelta = newPos - scrollPos;
 
-		elem.stem = true;
-		elem.speed = specialSpeed;
-	}
-	
-	self.css(elem.side, elem.sideOffset);
+			scrollPos = newPos;//Remember to update scrollPos
 
-	//set initial position, in case page wasn't at the top on load.
-	scrolling("firstPageLoad");
-	
-});
+			pageLoad ? initialScrollPos = newPos : 0;
 
-// Animation Logic -------------------------------
-function move(elem, self){
-	//Actually Move the element
-	if (elem.side === "left"){
-		self.stop(true, false).animate({left: elem.sideOffset}, 300, "linear");
-	}
-	else if (elem.side === "top") {
-		self.stop(true, false).animate({top: (elem.sideOffset)}, 300, "linear");
-	}
-	else {
-		self.stop(true, false).animate({right: elem.sideOffset}, 300, "linear");
-	}
-};
+			if (scrollDelta > 0 || pageLoad){ //Only move when scrolling down the page or on page load
+				moveLogic(scrollDelta, scrollPos);
+			}
+		};
 
-function moveLogic(scrollDelta, scrollPos){
-	movingElems.each(function(i, elem){
-		var self = $(this),
-			delay = 200; //for tuning bottle
-		
-							
-		if (elem.side === "top"){//bottle only logic
-						if (elem.stem){
-							console.log("scrollDelta" + scrollDelta);
-							console.log("sideOffset" + elem.sideOffset);
-							console.log("maxOffset" + elem.maxOffset);
-							console.log("startDistance" + elem.startDistance);
-							console.log("scrollPos" + scrollPos);
-						}
-			//delay before moving bottle and stem
-			if (scrollPos > 300){
-				elem.sideOffset += (scrollDelta * specialSpeed);
+		movingElems.each(function(i, elem){
+			var self = $(this);
+			
+			var initial = self.offset().top - timingValue;
 
-				if (elem.sideOffset < 0){elem.sideOffset = 0;}
+			if ( self.data("wait") ){initial += self.data("wait");}
+			//get data attribute for extra offset?
+			//Calculate original position based on dist from top of page?
+			elem.sideOffset = elem.maxOffset = initial * -1;
+			elem.startDistance = initial;
+			elem.speed = 1;
+			//Set the left/right offset of the element based on its topOffset
+			elem.side = self.hasClass("left") ? "left" : "right";
 
-				if (elem.sideOffset > elem.maxOffset){
-					specialSpeed = .7;
-					elem.sideOffset = elem.maxOffset;
+			//Initialize an "extraOffset" property for keeping track past 0
+			elem.extraOffset = 0;
+			elem.sideOffset = elem.maxOffset = initial * -1; //!!This is hardcoded to get around a bug.
+			
+			//only for the bottle
+			if (self.hasClass("bottle")){
+				elem.side = "top";
+				if ( self.is("#bottle") ){
+					elem.startDistance = 1600;
+					elem.sideOffset = 1600;
+					elem.maxOffset = 2120;
+					elem.speed = specialSpeed;
 				}
+			}
+			if ( self.is("#stem") ){
+				elem.maxOffset = 215;
+
+				elem.stem = true;
+				elem.speed = specialSpeed;
+			}
+			
+			self.css(elem.side, elem.sideOffset);
+
+			//set initial position, in case page wasn't at the top on load.
+			scrolling("firstPageLoad");
+		});
+		// Animation Logic -------------------------------
+		function move(elem, self){
+			//Actually Move the element
+			if (elem.side === "left"){
+				self.stop(true, false).animate({left: elem.sideOffset}, 300, "linear");
+			}
+			else if (elem.side === "top") {
+				self.stop(true, false).animate({top: (elem.sideOffset)}, 300, "linear");
+			}
+			else {
+				self.stop(true, false).animate({right: elem.sideOffset}, 300, "linear");
+			}
+		};
+
+		function moveLogic(scrollDelta, scrollPos){
+			movingElems.each(function(i, elem){
+				var self = $(this),
+					delay = 200; //for tuning bottle
+				
+									
+				if (elem.side === "top"){//bottle only logic
+								
+					//delay before moving bottle and stem
+					if (scrollPos > 300){
+						elem.sideOffset += (scrollDelta * specialSpeed); //So I can speed bottle up after stem stops moving
+
+						if (elem.sideOffset < 0){elem.sideOffset = 0;}
+
+						if (elem.sideOffset > elem.maxOffset){
+							specialSpeed = .7;
+							elem.sideOffset = elem.maxOffset;
+						}
+
+							move(elem, self);
+					}
+				}
+				else {
+					elem.sideOffset += initialScrollPos;
+
+					elem.sideOffset += scrollDelta;
+
+					if (elem.sideOffset > 0){elem.sideOffset = 0;}
+
+					if (elem.sideOffset < elem.maxOffset){elem.sideOffset = elem.maxOffset;}
 
 					move(elem, self);
-			}
-		}
-		else {
-			elem.sideOffset += initialScrollPos;
-
-			elem.sideOffset += scrollDelta;
-
-			if (elem.sideOffset > 0){elem.sideOffset = 0;}
-
-			if (elem.sideOffset < elem.maxOffset){elem.sideOffset = elem.maxOffset;}
-
-			move(elem, self);
-		}
-	})
-};
+				}
+			})
+		};
 
 
-$(window).scroll(function (){ 
-	scrolling();
+		$(window).scroll(function (){ 
+			scrolling();
+		});
+	};
+
+	/*****************/
+	//Test first
+	/*var testElem = $("#startedText"),
+		testElemPos = parseInt(testElem.offset().top);
+
+	testElem.animate({left: testElemPos}, 0, "linear");
+	testNewPos = parseInt( testElem.css("left") );
+	if (testElemPos === testNewPos){
+		//Begin
+		initParallax();
+	}
+	*/
+	initParallax();
+
 });
